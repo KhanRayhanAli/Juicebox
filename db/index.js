d84f11df248ed1dfd7ab30aebca7997c0e4ab7c7
Brevan <brev.bdawg@gmail.com>
2022-10-31 13:51:09 -0500

    workshop 2.2 completed

--- db/index.js
+++ db/index.js
@@ -314,6 +314,20 @@ async function getPostsByTagName(tagName) {
   }
 }
 
+async function getUserByUsername(username) {
+  try {
+    const { rows: [user] } = await client.query(`
+    SELECT * 
+    FROM users
+    WHERE username=$1;
+    `, [username]);
+
+    return user;
+    } catch (error) {
+      throw error;
+    }
+}
+
 module.exports = {
   client,
   getAllUsers,
@@ -328,4 +342,6 @@ module.exports = {
   addTagsToPost,
   getPostById,
   getPostsByTagName,
+  getUserByUsername,
+  
 };
--- index.js
+++ index.js
@@ -1,27 +1,27 @@
+require("dotenv").config();
+
 const PORT = 3000;
-const express = require('express');
+const express = require("express");
 const server = express();
-const morgan = require('morgan');
-server.use(morgan('dev'));
-
-
-
-const apiRouter = require('./api');
-server.use('/api', apiRouter);
-
-const { client } = require('./db');
-client.connect();
-
-server.listen(PORT, () => {
-    console.log('The server is active on port', PORT)
-});
+const morgan = require("morgan");
+server.use(morgan("dev"));
 
 server.use(express.json());
 
 server.use((req, res, next) => {
-    console.log("<____Body Logger START____>");
-    console.log(req.body);
-    console.log("<____Body Logger END____>");
+  console.log("<____Body Logger START____>");
+  console.log(req.body);
+  console.log("<____Body Logger END____>");
 
-    next();
-})
+  next();
+});
+
+const apiRouter = require("./api");
+server.use("/api", apiRouter);
+
+const { client } = require("./db");
+client.connect();
+
+server.listen(PORT, () => {
+  console.log("The server is active on port", PORT);
+});
